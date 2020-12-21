044a2c8278e587a0ecc1225bb630497999f2ca11
rayukencalculator <76089851+rayukencalculator@users.noreply.github.com>
2020-12-21 11:20:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+window.alert("Welcome To Rayuken's Calculator!")
+        var am=window.prompt("Write 'A' for Addition 'M' For Minus 'D' To Divide Or 'MQ' To Multiply")
+        var Y
+        var X
+        var Z
+        if (am=='A'){Y=window.prompt("Write The First Number To Add!")
+        X=window.prompt("Write The Second Number To Add!")
+        document.write("Your Answer is:",Z=parseInt(Y)+parseInt(X))}
+        if (am=='M'){Y=window.prompt("Write The First Number To Minus!")
+        X=window.prompt("Write The Second Number To Minus!")
+        document.write("Your Answer is:",Z=parseInt(Y)-parseInt(X))}
+        if (am=='D'){Y=window.prompt("Write The First Number To Divide!")
+        X=window.prompt("Write The Second Number To Divide!")
+        document.write("Your Answer is:",Z=parseInt(Y)/parseInt(X))}
+        if (am=='MQ'){Y=window.prompt("Write The First Number To Multiply!")
+        X=window.prompt("Write The Second Number To Multiply!")
+        document.write("Your Answer is:",Z=parseInt(Y)*parseInt(X))}
+        if (am=='a'){Y=window.prompt("Write The First Number To Add!")
+        X=window.prompt("Write The Second Number To Add!")
+        document.write("Your Answer is:",Z=parseInt(Y)+parseInt(X))}
+        if (am=='m'){Y=window.prompt("Write The First Number To Minus!")
+        X=window.prompt("Write The Second Number To Minus!")
+        document.write("Your Answer is:",Z=parseInt(Y)-parseInt(X))}
+        if (am=='d'){Y=window.prompt("Write The First Number To Divide!")
+        X=window.prompt("Write The Second Number To Divide!")
+        document.write("Your Answer is:",Z=parseInt(Y)/parseInt(X))}
+        if (am=='mq'){Y=window.prompt("Write The First Number To Multiply!")
+        X=window.prompt("Write The Second Number To Multiply!")
+        document.write("Your Answer is:",Z=parseInt(Y)*parseInt(X))}
+        if (Y=='69'){window.alert("nice....(im cultured calculator)")}
+        if (X=='69'){window.alert("nice....(im cultured calculator)")}
+        if (Y=='69420'){window.alert("nice....(im cultured calculator)")}
+        if (X=='69420'){window.alert("nice....(im cultured calculator)")}
+        if (Y=='42069'){window.alert("nice....(im cultured calculator)")}
+        if (X=='42069'){window.alert("nice....(im cultured calculator)")}
+        if (Z=='69'){document.write(" nice....(im cultured calculator)")}
+        if (Z=='42069'){document.write(" nice....(im cultured calculator")}
+        if (Z=='69420'){document.write(" nice....(im cultured calculator")}
